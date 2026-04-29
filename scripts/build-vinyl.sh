435d22bbf458836f544c670b62064a43b88b8862
hibaarsi <hiba.arsi@alumnos.upm.es>
2026-04-29 18:03:18 +0200

    si

--- scripts/build-vinyl.sh
+++ scripts/build-vinyl.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "🔌 Conectando con el demonio de Docker de Minikube..."
+
+# shellcheck disable=SC2046
 eval $(minikube docker-env)
 
 echo "🏗️ Construyendo imagen: vinyl-front:1.0.0..."
